3cf0475ad004ca06c5b2fb82c7810188f5abb716
justus <justg2007@gmail.com>
2025-09-30 17:32:12 +0200

    2

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,7 @@ function App() {
 
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <Home />
     </>
   )
 }
